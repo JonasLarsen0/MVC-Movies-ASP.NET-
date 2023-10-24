e87ae869ce25060b9abdf72e2ec80cb6d3d0bebf
JonasL <46361162+JonasLarsen0@users.noreply.github.com>
2023-10-24 09:29:53 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MoviesWebApp.Models;
+
+namespace MoviesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MoviesWebApp.Models.Movie>? Movie { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace MoviesWebApp.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Runtime { get; set; }
+
+        public Movie() 
+        { 
+        
+        }
+    }
+}
